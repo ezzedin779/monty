76b799e9bf344a4ece8cfc9af9144b1ee6e493ae
ezzedin779 <abdou.babchia64@gmail.com>
2020-12-22 23:54:13 +0100

    HEADER 0.2

--- monty.h
+++ monty.h
@@ -21,8 +21,8 @@ extern char **tokens;
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+		char *opcode;
+		void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -35,9 +35,35 @@ typedef struct instruction_s
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+		int n;
+		struct stack_s *prev;
+		struct stack_s *next;
 } stack_t;
+
+char *get_int(int num);
+unsigned int _abs(int i);
+int base_len(unsigned int num, unsigned int base);
+void fill_buffer(unsigned int num, unsigned int base,
+					char *buff, int buff_size);
+int int_error(unsigned int line_number);
+int short_error(unsigned int line_number, char *op);
+int div_error(unsigned int line_number);
+int pop_error(unsigned int line_number);
+int pint_error(unsigned int line_number);
+void _error_choice(int error_code);
+void _push(stack_t **stack, unsigned int line_number);
+void _nop(stack_t **stack, unsigned int line_number);
+void _pall(stack_t **stack, unsigned int line_number);
+void _pint(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
+int malloc_error(void);
+int init_stack(stack_t **stack);
+void _add(stack_t **stack, unsigned int line_number);
+void _sub(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
+void _mod(stack_t **stack, unsigned int line_number);
+char *next(char *str, char *delims);
+
 
 #endif
